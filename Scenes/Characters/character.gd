ed6d5934aec679667fac606641f12b1e22070813
Hugo <haaa20@sussex.ac.uk>
2024-03-14 15:03:35 +0000

    ready for Kingsley pray for me

--- Scenes/Characters/character.gd
+++ Scenes/Characters/character.gd
@@ -4,29 +4,32 @@ class_name Character
 const TERMINAL_VELOCITY = 300
 
 @export var speed = 12
+@export var buddy: Character
+@export var currently_controlled = true
+
 var face_right = true
 var grav_excempt = false
-var currently_controlled = true
 var grav_v = 0.0
 var anim: Anim
+
 @onready var sprite = $sprite
+@onready var cam = $cam
 @onready var woosh_time = Timer.new()
+@onready var pass_control_buffer = Timer.new()
+
 
 # Animation enum
 enum Anim {IDLE, RUN, AIR_UP, AIR_DOWN}
 
 func move_by_input(v: Vector2) -> Vector2:
 	var vn = v
-	var idle = true
 	vn.x = 0
 	
 	if Input.is_action_pressed("ui_left"):
 		vn.x -= 1
-		idle = false
 		anim = Anim.RUN
 	if Input.is_action_pressed("ui_right"):
 		vn.x += 1
-		idle = false
 		anim = Anim.RUN
 	
 	if Input.is_action_just_pressed("ui_right"):
@@ -38,9 +41,6 @@ func move_by_input(v: Vector2) -> Vector2:
 	
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		vn = jump(vn)
-	
-	if idle:
-		anim = Anim.IDLE
 	
 	vn.x *= speed * 15
 	return vn
@@ -77,6 +77,10 @@ func apply_gravity(v: Vector2) -> Vector2:
 	# Animation
 	if !is_on_floor() and velocity.y > 0:
 		anim = Anim.AIR_DOWN
+	elif velocity.y < 0:
+		anim = Anim.AIR_UP
+	elif velocity == Vector2.ZERO:
+		anim = Anim.IDLE
 	
 	return vn
 		
@@ -89,18 +93,52 @@ func update_animation():
 			sprite.play("run")
 		Anim.AIR_DOWN:
 			sprite.play("air_down")
+		Anim.AIR_UP:
+			sprite.play("air_up")
+
+
+func pass_control(to: Character):
+	# Just a safeguard:
+	if !currently_controlled: return
+	print(self.name + " passing control")
+	currently_controlled = false
+
+	# Stop any horizontal movement
+	velocity.x = 0
 	
+	# Start the timer
+	pass_control_buffer.wait_time = 0.1
+	pass_control_buffer.timeout.connect(to.recieve_control.bind(self))
+	pass_control_buffer.start()
+
+	# Swap the camera
+	to.cam.enabled = true
+	cam.enabled = false
+
+
+func recieve_control(from: Character):
+	print(self.name + " recieveing control")
+	currently_controlled = true
+	from.pass_control_buffer.timeout.disconnect(recieve_control)
+
 
 func _process(delta):
-	velocity =  move_by_input(velocity)
+	if currently_controlled:
+		velocity = move_by_input(velocity)
+		if Input.is_action_just_pressed("ui_accept"): pass_control(buddy)
+	else:
+		pass
+	
 	velocity = apply_gravity(velocity)
 	update_animation()
-
 	move_and_slide()
 
 
 func _ready():
 	woosh_time.one_shot = true
 	add_child(woosh_time)
+	add_child(pass_control_buffer)
 	woosh_time.timeout.connect(woosh_time_over)
+	pass_control_buffer.one_shot = true
+	cam.enabled = currently_controlled
 	
