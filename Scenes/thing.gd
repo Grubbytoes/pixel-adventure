951a5f758b0beb2257310d867f25c4e0f8e1f712
hughie <=>
2024-03-18 16:45:22 +0000

    staging and unstaging now tested

--- Scenes/thing.gd
+++ Scenes/thing.gd
@@ -7,16 +7,17 @@ var player_character: Character
 
 func to_frog():
 	player_character = $ninja_frog
+	GoConductor.unstage("mystage")
 	
 
 func to_vr_guy():
 	player_character = $virtual_guy
+	GoConductor.load_stage("mystage")
 
 
 func _ready():
 	$virtual_guy.currently_controlled = true
 	to_vr_guy()
-	GoConductor.load_stage("mystage")
 
 
 func _process(_delta):
