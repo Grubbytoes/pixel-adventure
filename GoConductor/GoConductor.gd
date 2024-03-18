00a7a49e4028ff1db6aa583eb2180ae989ddba3d
hughie <=>
2024-03-18 18:27:18 +0000

    Added swap_stage and get_stage

--- GoConductor/GoConductor.gd
+++ GoConductor/GoConductor.gd
@@ -5,6 +5,8 @@ extends Node2D
 func say_hello():
     print("Hello, from GoConductor!")
 
+func get_stage(stage_name) -> Node:
+    return _stages.get(stage_name)
 
 ## Loads the stage of the given name from the stages directory. Returns true if successful
 ## Path does NOT need to include '.tsct' or the root stages directory
@@ -17,14 +19,9 @@ func load_stage(stage_name: String, pseudonym:String="") -> bool:
     if _stages.has(stage_name):
         printerr("GoConductor: Tried to load a stage of stage_name '"+stage_name+"' when one is already loaded")
         return false
-
-    # load the stage, and make sure there's no error
-    var newstage = load(path).instantiate()
-    if newstage is Error:
-        printerr("GoCOnductor: there was an error when loading stage '"+stage_name+"'")
-        return false
     
     # Add the stage
+    var newstage = _instance_stage(path)
     add_child(newstage)
     _stages[stage_name] = newstage
     return true
@@ -38,8 +35,27 @@ func unstage(stage_name: String) -> bool:
 
 
 func swap_stage(new_stage_name: String, stage_name: String) -> bool:
-    return false
+    if !_stages.has(stage_name):
+        return false
+    
+    # Assigning variables
+    var old_stage = _stages[stage_name]
+    var new_stage = _instance_stage(_to_path(new_stage_name))
 
+    # Swapping over the nodes
+    add_child(new_stage)
+    _stages[stage_name] = new_stage
+    old_stage.queue_free()
+
+    # Done!
+    return true
+    
 
 func _to_path(somestr: String) -> String:
     return "res://GoConductor/Stages/" + somestr + ".tscn"
+
+
+func _instance_stage(path: String) -> Node:
+    var s = load(path)
+    return s.instantiate()
+    
