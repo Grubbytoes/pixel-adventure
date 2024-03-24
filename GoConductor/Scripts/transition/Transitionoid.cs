261b6403f4617d2e6b6d7cc4e1bbef95c10aa4fd
Hugo <haaa20@sussex.ac.uk>
2024-03-24 13:01:16 +0000

    Transitionoids
    * Now serve as a container for a music controller
    *Serving as an aditional pointer to a music track, if they contain one

--- GoConductor/Scripts/transition/Transitionoid.cs
+++ GoConductor/Scripts/transition/Transitionoid.cs
@@ -1,11 +1,49 @@
 ﻿namespace GoConductor;
 
 /// <summary>
-/// Lmao IK Transitionoid isn't a word leave me alone.
-///
-/// In this context it means "That which is effected by a transition. If you can think of a better word for that HMU
+/// As in "That which can be affected by a transition"
 /// </summary>
-public interface ITransitionoid
+public class Transitionoid: IMusicController
 {
-    
+    public IMusicController Target { set; get; }
+    public MusicTrack TerminalTrack { set; get; }
+
+    // Transitionoid containing a generic MusicController
+    public Transitionoid(IMusicController target)
+    {
+        Target = target;
+        TerminalTrack = null;
+    }
+
+    // Transitionoid containing a Track - ie a guaranteed 'leaf' with no further children
+    public Transitionoid(MusicTrack target)
+    {
+        Target = target;
+        TerminalTrack = target;
+    }
+
+    public void Play()
+    {
+        Target.Play();
+    }
+
+    public void Pause()
+    {
+        Target.Pause();
+    }
+
+    public void Stop()
+    {
+        Target.Stop();
+    }
+
+    public void Restart()
+    {
+        Target.Restart();
+    }
+
+    public void TogglePause()
+    {
+        Target.TogglePause();
+    }
 }
